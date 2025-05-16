de1652773838c051610ee7512b8d2a64e8b2ab5c
Apu Rayhan <mdapurayhan1729@gmail.com>
2025-05-16 15:19:21 +0600

    kdfhjh

--- src/Main.java
+++ src/Main.java
@@ -316,7 +316,69 @@ import java.awt.*;
 //    }
 //}
 
+class Dog {
+    // Attributes (Fields) - Kept private for encapsulation
+    private String name;
+    private int age;
+    private String breed;
 
+    // Constructor
+    public Dog(String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+
+    // Behaviors (Methods)
+    public void bark() {
+        System.out.println(name + " is barking! Woof Woof!");
+    }
+
+    public void spin() {
+        System.out.println(name + " spins around in a circle!");
+    }
+
+    public void run() {
+        System.out.println(name + " is running fast!");
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    // Setter methods
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setAge(int age) {
+//        if (age >= 0) {
+//            this.age = age;
+//        } else {
+//            System.out.println("Age can't be negative.");
+//        }
+//    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    // Optional: display dog info
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age + " years");
+        System.out.println("Breed: " + breed);
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
